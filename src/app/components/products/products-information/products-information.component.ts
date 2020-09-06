47064052dedef0a11933accf12113aa4c5997d19
iryna_l <irynal@tallium.com>
2020-09-06 23:50:23 +0300

    first commit

---
+++ src/app/components/products/products-information/products-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-information',
+  templateUrl: './products-information.component.html',
+  styleUrls: ['./products-information.component.scss']
+})
+export class ProductsInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
